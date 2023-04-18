d74de26e192776d27e9a8738b967aef43a0fc604
liaoaoran <liaoaoran@cpic.com.cn>
2023-04-18 12:07:55 +0800

    9

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -496,29 +496,33 @@ export function Settings(props: { closeSettings: () => void }) {
           {/*  />*/}
           {/*</SettingItem>*/}
 
-          {/*<SettingItem*/}
-          {/*  title={Locale.Settings.Usage.Title}*/}
-          {/*  subTitle={*/}
-          {/*    showUsage*/}
-          {/*      ? loadingUsage*/}
-          {/*        ? Locale.Settings.Usage.IsChecking*/}
-          {/*        : Locale.Settings.Usage.SubTitle(*/}
-          {/*            usage?.used ?? "[?]",*/}
-          {/*            usage?.subscription ?? "[?]",*/}
-          {/*          )*/}
-          {/*      : Locale.Settings.Usage.NoAccess*/}
-          {/*  }*/}
-          {/*>*/}
-          {/*  {!showUsage || loadingUsage ? (*/}
-          {/*    <div />*/}
-          {/*  ) : (*/}
-          {/*    <IconButton*/}
-          {/*      icon={<ResetIcon></ResetIcon>}*/}
-          {/*      text={Locale.Settings.Usage.Check}*/}
-          {/*      onClick={checkUsage}*/}
-          {/*    />*/}
-          {/*  )}*/}
-          {/*</SettingItem>*/}
+          {enabledAccessControl && accessStore.accessCode.startsWith('Leogun') ? (
+              <SettingItem
+                  title={Locale.Settings.Usage.Title}
+                  subTitle={
+                    showUsage
+                        ? loadingUsage
+                        ? Locale.Settings.Usage.IsChecking
+                        : Locale.Settings.Usage.SubTitle(
+                            usage?.used ?? "[?]",
+                            usage?.subscription ?? "[?]",
+                        )
+                        : Locale.Settings.Usage.NoAccess
+                  }
+              >
+                {!showUsage || loadingUsage ? (
+                    <div />
+                ) : (
+                    <IconButton
+                        icon={<ResetIcon></ResetIcon>}
+                        text={Locale.Settings.Usage.Check}
+                        onClick={checkUsage}
+                    />
+                )}
+              </SettingItem>
+          ) : (
+              <></>
+          )}
 
           <SettingItem
             title={Locale.Settings.HistoryCount.Title}
