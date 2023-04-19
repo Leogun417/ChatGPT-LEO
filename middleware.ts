4933ca26d7d5e5570828b0e0f62ad8662efde854
Leogun417 <1019717244@qq.com>
2023-04-19 23:19:03 +0800

    fanhuicuowu4

--- middleware.ts
+++ middleware.ts
@@ -1,97 +1,96 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getServerSideConfig } from "./app/config/server";
+import {NextRequest, NextResponse} from "next/server";
+import {getServerSideConfig} from "./app/config/server";
 import md5 from "spark-md5";
 import * as jose from "node-jose";
 
 export const config = {
-  matcher: ["/api/openai", "/api/chat-stream"],
+    matcher: ["/api/openai", "/api/chat-stream"],
 };
 
 const serverConfig = getServerSideConfig();
 
-function verifyJwtToken(jwtToken: string): boolean {
-    if (!jwtToken || jwtToken.trim().length === 0) {
-        return false;
-    }
+async function verifyJwtToken(jwtToken: string): Promise<jose.JWS.VerificationResult> {
     // 获取公钥
-    const key = jose.JWK.asKey({
+    const key = await jose.JWK.asKey({
         kty: 'RSA',
         n: serverConfig.n,
         e: serverConfig.e
     });
 
-    try {
     // 校验JWT Token
-    jose.JWS.createVerify(key)
+    return jose.JWS.createVerify(key)
         .verify(jwtToken);
-    } catch(err) {
-        return false;
-    }
-    return true;
 }
 
 
 function getIP(req: NextRequest) {
-  let ip = req.ip ?? req.headers.get("x-real-ip");
-  const forwardedFor = req.headers.get("x-forwarded-for");
+    let ip = req.ip ? ? req.headers.get("x-real-ip");
+    const forwardedFor = req.headers.get("x-forwarded-for");
 
-  if (!ip && forwardedFor) {
-    ip = forwardedFor.split(",").at(0) ?? "";
-  }
+    if (!ip && forwardedFor) {
+        ip = forwardedFor.split(",").at(0) ? ? "";
+    }
 
-  return ip;
+    return ip;
 }
 
 export function middleware(req: NextRequest) {
-  const accessCode = req.headers.get("access-code");
-  const token = req.headers.get("token");
-  const hashedCode = md5.hash(accessCode ?? "").trim();
+    const accessCode = req.headers.get("access-code");
+    const token = req.headers.get("token");
+    const hashedCode = md5.hash(accessCode ? ? "").trim();
 
-  console.log("[Auth] allowed hashed codes: ", [...serverConfig.codes]);
-  console.log("[Auth] got access code:", accessCode);
-  console.log("[Auth] hashed access code:", hashedCode);
-  console.log("[User IP] ", getIP(req));
-  console.log("[Time] ", new Date().toLocaleString());
+    console.log("[Auth] allowed hashed codes: ", [...serverConfig.codes]);
+    console.log("[Auth] got access code:", accessCode);
+    console.log("[Auth] hashed access code:", hashedCode);
+    console.log("[User IP] ", getIP(req));
+    console.log("[Time] ", new Date().toLocaleString());
 
-  if (serverConfig.needCode
-      && !serverConfig.codes.has(hashedCode)
-      && !verifyJwtToken(accessCode) && !token) {
-    return NextResponse.json(
-      {
-        error: true,
-        needAccessCode: true,
-        msg: "Please go settings page and fill your access code.",
-      },
-      {
-        status: 401,
-      },
-    );
-  }
-
-  // inject api key
-  if (!token) {
-    const apiKey = serverConfig.apiKey;
-    if (apiKey) {
-      console.log("[Auth] set system token");
-      req.headers.set("token", apiKey);
-    } else {
-      return NextResponse.json(
-        {
-          error: true,
-          msg: "Empty Api Key",
-        },
-        {
-          status: 401,
-        },
-      );
+    let validToken = true;
+    try {
+        verifyJwtToken(accessCode);
+    } catch (err) {
+        validToken = false;
     }
-  } else {
-    console.log("[Auth] set user token");
-  }
 
-  return NextResponse.next({
-    request: {
-      headers: req.headers,
-    },
-  });
+    if (serverConfig.needCode
+        && !serverConfig.codes.has(hashedCode)
+        && !validToken && !token) {
+        return NextResponse.json(
+            {
+                error: true,
+                needAccessCode: true,
+                msg: "Please go settings page and fill your access code.",
+            },
+            {
+                status: 401,
+            },
+        );
+    }
+
+    // inject api key
+    if (!token) {
+        const apiKey = serverConfig.apiKey;
+        if (apiKey) {
+            console.log("[Auth] set system token");
+            req.headers.set("token", apiKey);
+        } else {
+            return NextResponse.json(
+                {
+                    error: true,
+                    msg: "Empty Api Key",
+                },
+                {
+                    status: 401,
+                },
+            );
+        }
+    } else {
+        console.log("[Auth] set user token");
+    }
+
+    return NextResponse.next({
+        request: {
+            headers: req.headers,
+        },
+    });
 }
