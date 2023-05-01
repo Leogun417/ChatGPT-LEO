648dc86e0fb4ae47d48a083ad79b325050dc08de
Leogun417 <1019717244@qq.com>
2023-05-02 00:07:09 +0800

    移动端处理

--- app/token.tsx
+++ app/token.tsx
@@ -2,6 +2,47 @@ import React, { useState } from "react";
 import axios from "axios";
 import { getHeaders } from "./requests";
 
+const isMobile =
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent,
+  );
+
+const copyToClipboard = async (text) => {
+  try {
+    if (!text) return;
+    if (isMobile) {
+      const tempInput = document.createElement("input");
+      tempInput.style.position = "absolute";
+      tempInput.style.left = "-1000px";
+      tempInput.style.top = "-1000px";
+      tempInput.value = text;
+      document.body.appendChild(tempInput);
+      const range = document.createRange();
+      range.selectNodeContents(tempInput);
+      const selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+      tempInput.setSelectionRange(0, 99999); // 移动端必须手动选中文本
+      document.execCommand("copy");
+      document.body.removeChild(tempInput);
+    } else if (navigator.clipboard && navigator.clipboard.writeText) {
+      await navigator.clipboard.writeText(text);
+    } else {
+      const tempInput = document.createElement("input");
+      tempInput.style.position = "fixed";
+      tempInput.style.opacity = "0";
+      tempInput.value = text;
+      document.body.appendChild(tempInput);
+      tempInput.select();
+      document.execCommand("copy");
+      document.body.removeChild(tempInput);
+    }
+    alert("Text copied to clipboard!");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export default function TokenPage() {
   const [inputValue, setInputValue] = useState("");
   const handleSubmit = async () => {
@@ -14,21 +55,7 @@ export default function TokenPage() {
         },
       );
       const tokenValue = response.data.token;
-      if (navigator.clipboard && navigator.clipboard.writeText) {
-        await navigator.clipboard.writeText(tokenValue);
-        alert("Token copied to clipboard!");
-      } else {
-        // 如果浏览器不支持 Clipboard API，则使用 document.execCommand('copy') 方法
-        const tempInput = document.createElement("input");
-        tempInput.style.position = "fixed";
-        tempInput.style.opacity = "0";
-        tempInput.value = tokenValue;
-        document.body.appendChild(tempInput);
-        tempInput.select();
-        document.execCommand("copy");
-        document.body.removeChild(tempInput);
-        alert("Token copied to clipboard!");
-      }
+      copyToClipboard(tokenValue);
     } catch (error) {
       console.error(error);
     }
